495806d7634c70ff8bb2be46bc83d2c2d409ba72
iankhor <iankhorbc.thecoder@gmail.com>
2018-05-01 00:12:53 +1000

    seed directory

--- app/models/directory.rb
+++ app/models/directory.rb
@@ -1,2 +1,3 @@
 class Directory < ApplicationRecord
+  self.table_name = 'directory'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require_relative './seed_helper'
+Faker::Config.locale = 'en-AU'
+
+puts "*" * 10 + 'SEED START' + "*" * 10
+
+seed_directory(30000)
+
+puts "*" * 10 + 'SEED END' + "*" * 10
