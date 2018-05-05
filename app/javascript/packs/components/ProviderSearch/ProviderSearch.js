aecbe168bacaf82642f6b9d96a536f8c07a45f8c
iankhor <iankhorbc.thecoder@gmail.com>
2018-05-05 22:40:05 +1000

    add dummy table

--- app/javascript/packs/components/ProviderSearch/ProviderSearch.js
+++ app/javascript/packs/components/ProviderSearch/ProviderSearch.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Input, Segment, Header } from 'semantic-ui-react'
+import { Table, Container, Input, Segment, Header, Button } from 'semantic-ui-react'
 
 const FixedMenuLayout = () => (
   <div>
@@ -8,9 +8,38 @@ const FixedMenuLayout = () => (
         <Input fluid inverted icon='search' placeholder='Search...' />
       </Segment>
 
-      <Header as='h1'>Semantic UI React Fixed Template</Header>
-      <p>This is a basic fixed menu template using fixed size containers.</p>
-      <p>A text container is used for the main container, which is useful for single column layouts.</p>
+      <Segment>
+      <Table compact>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Provider Name</Table.HeaderCell>
+              <Table.HeaderCell>Practice Name</Table.HeaderCell>
+              <Table.HeaderCell>Practice Address</Table.HeaderCell>
+              <Table.HeaderCell>Practice Email</Table.HeaderCell>
+              <Table.HeaderCell>Practice Phone</Table.HeaderCell>
+              <Table.HeaderCell>Action</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>John Smith</Table.Cell>
+              <Table.Cell>Some Clinic</Table.Cell>
+              <Table.Cell>1 Infinity Loop, Melbourne</Table.Cell>
+              <Table.Cell>the@email.com</Table.Cell>
+              <Table.Cell>0312341234</Table.Cell>
+              <Table.Cell>
+                <Button.Group>
+                    <Button icon='play' />
+                    <Button icon='pause' />
+                    <Button icon='shuffle' />
+                </Button.Group>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+
+      </Segment>
 
     </Container>
   </div>
