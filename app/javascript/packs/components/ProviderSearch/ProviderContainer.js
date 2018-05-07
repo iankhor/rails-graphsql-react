4199b9d9047d5288715bb777abf5f764f5e76818
iankhor <iankhorbc.thecoder@gmail.com>
2018-05-07 23:37:40 +1000

    allow aysnc await , instal axios, prefetch data from graphql, disable csrf

--- app/javascript/packs/components/ProviderSearch/ProviderContainer.js
+++ app/javascript/packs/components/ProviderSearch/ProviderContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ProviderTable from './ProviderSearch'
 import faker from 'faker'
+import axios from 'axios'
 
 const data = [
   {
@@ -20,6 +21,18 @@ const data = [
 ]
 
 export default class ProviderContainer extends Component {
+
+  componentDidMount = () => {
+    this.getProviders()
+  }
+
+  getProviders = async () => {
+    const query = 'query {\n  directory(\n    first: 20,\n    skip: 0\n  ) {\n    country_code\n    email\n    first_name\n    last_name\n    phone\n    title\n  } }'
+
+    const { data } = await axios.post('/graphql', { query })
+    console.log(data)
+  }
+
   render() {
     return(
       <div>
