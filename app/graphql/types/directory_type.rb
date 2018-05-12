fdd11d1c7936d3fe047adeda7e4acab10fcde210
iankhor <iankhorbc.thecoder@gmail.com>
2018-05-12 16:06:09 +1000

    remove not needed fields

--- app/graphql/types/directory_type.rb
+++ app/graphql/types/directory_type.rb
@@ -1,14 +1,9 @@
 Types::DirectoryType = GraphQL::ObjectType.define do
   name 'Directory'
 
-  field :title, types.String
-  field :first_name, types.String
-  field :last_name, types.String
+  field :full_address, types.String
+  field :full_name, types.String
   field :gender, types.String
   field :phone, types.String
   field :email, types.String
-  field :latitude, types.String
-  field :longitude, types.String
-  field :full_address, types.String
-  field :full_name, types.String
 end
