4de013d3e0e18270c93fdf084df54c974dc1e663
iankhor <iankhorbc.thecoder@gmail.com>
2018-05-12 16:06:43 +1000

    remove comments

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,19 +1,12 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
 
-  # TODO: remove me
   field :testField, types.String do
     description "An example field added by the generator"
     resolve ->(obj, args, ctx) {
       "Hello World!"
     }
   end
-
-  # field :directory, !types[Types::DirectoryType] do
-  #   resolve -> (obj, args, ctx) {
-  #     Directory.order(:created_at).last(5)
-  #   }
-  # end
 
   field :directory, function: Resolvers::ProviderSearch
 end
