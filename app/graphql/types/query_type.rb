1d381c2e4acc9de3dc43f31f069a6ad3c0666d10
iankhor <iankhorbc.thecoder@gmail.com>
2018-05-12 17:30:53 +1000

    clean up

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,18 +1,10 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
 
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
-  end
-
+  # example without connections
   field :directory, function: Resolvers::ProviderSearch
 
   connection :directoryConnection, Connections::DirectoryConnection do
-    argument :first, types.Int, default_value: 20
-
     resolve ->(_obj, _args, _ctx) { Directory.all.order('last_name') }
   end
 
