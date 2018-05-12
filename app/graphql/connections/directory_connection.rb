5b7aa8a824da33e1ce3590f510b86ee321f15cd8
iankhor <iankhorbc.thecoder@gmail.com>
2018-05-12 16:51:07 +1000

    basic total pages

--- app/graphql/connections/directory_connection.rb
+++ app/graphql/connections/directory_connection.rb
@@ -6,6 +6,15 @@ Connections::DirectoryConnection = Types::DirectoryType.define_connection do
 
     resolve ->(obj, _args, _ctx) {
       obj.nodes.count
+
+    }
+  end
+
+  field :totalPages do
+    type types.Int
+
+    resolve ->(obj, _args, _ctx) {
+      obj.nodes.count / 10
     }
   end
 end
