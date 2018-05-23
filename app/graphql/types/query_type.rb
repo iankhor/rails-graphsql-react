8addf4f6d4596af5ca448f45060582db7cf48830
iankhor <iankhorbc.thecoder@gmail.com>
2018-05-23 21:18:03 +1000

    return regardless of search_term present

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,8 +8,8 @@ Types::QueryType = GraphQL::ObjectType.define do
     argument :search_term, types.String
 
     resolve ->(_obj, args, _ctx) {
-       Directory.all.order(:last_name, :first_name) unless args[:search_term].present?
-       Directory.search_name(args[:search_term]) if args[:search_term].present?
+       return Directory.search_name(args[:search_term]) if args[:search_term].present?
+       return Directory.all.order(:last_name, :first_name)
      }
   end
 
