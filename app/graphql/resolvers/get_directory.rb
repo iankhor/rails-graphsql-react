8032d4c00e20e01d12666410819e8ffa81bd4d10
iankhor <iankhorbc.thecoder@gmail.com>
2018-05-23 23:31:57 +1000

    refactor resolver for directory

---
+++ app/graphql/resolvers/get_directory.rb
@@ -0,0 +1,14 @@
+module Resolvers
+  class GetDirectory
+    def call(_, args, _)
+      directory(args)
+    end
+
+    private
+
+    def directory(args)
+      return Directory.search_name(args[:searchTerm]) if args[:searchTerm].present?
+      return Directory.all.order(:last_name, :first_name)
+    end
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,11 +6,7 @@ Types::QueryType = GraphQL::ObjectType.define do
 
   connection :directory, Connections::DirectoryConnection do
     argument :searchTerm, types.String
-
-    resolve ->(_obj, args, _ctx) {
-       return Directory.search_name(args[:searchTerm]) if args[:searchTerm].present?
-       return Directory.all.order(:last_name, :first_name)
-     }
+    
+    resolve Resolvers::GetDirectory.new
   end
-
 end
