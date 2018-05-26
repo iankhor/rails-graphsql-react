ffcbcc5fe8e0c179732d6bbae7a369582dbdb94f
iankhor <iankhorbc.thecoder@gmail.com>
2018-05-26 10:17:55 +1000

    placehodler for update provider

---
+++ app/graphql/resolvers/update_provider.rb
@@ -0,0 +1,23 @@
+class Resolvers::UpdateProvider < GraphQL::Function
+  argument :id, types.Int
+  argument :title, types.String
+  argument :first_name, types.String
+  argument :last_name, types.String
+  argument :gender, types.String
+  argument :phone, types.String
+  argument :email, types.String
+  argument :street_line_1, types.String
+  argument :street_line_2, types.String
+  argument :sublocality, types.String
+  argument :locality, types.String
+  argument :postal_code, types.String
+  argument :country_code, types.String
+  argument :latitude, types.String
+  argument :longitude, types.String
+
+  type Types::DirectoryType
+
+  def call(_obj, args, _ctx)
+    Directory.find(args[:id])
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,13 +1,6 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
-  # TODO: Remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
-  end
-
   field :createProvider, function: Resolvers::CreateProvider.new
+  field :updateProvider, function: Resolvers::UpdateProvider.new
 end
