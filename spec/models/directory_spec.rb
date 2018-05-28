09b1f97a55e428ea86e755c0bae416df87c1b84a
iankhor <iankhorbc.thecoder@gmail.com>
2018-05-28 23:39:24 +1000

    detele unwanted files

--- spec/models/directory_spec.rb
+++ spec/models/directory_spec.rb
@@ -1,9 +1,29 @@
-RSpec.describe Directory, type: :model do
-  let!(:directory) { create(:directory) }
+require 'rails_helper'
 
-  describe 'example ' do
-    it 'errors' do
-      expect(directory.first_name).to eq directory.last_name
-    end
+RSpec.describe Directory, type: :model do
+  let(:provider) {
+    create(
+      :directory,
+      title: 'Mr',
+      first_name: 'Bright',
+      last_name: 'Side',
+      street_line_1: 'Little Street',
+      street_line_2: 'Around the corner',
+      sublocality: 'Westin',
+      locality: 'Hotel',
+      postal_code: '1111',
+      country_code: 'au'
+    )
+  }
+  let(:full_name) { provider.full_name }
+  let(:full_address) { provider.full_address }
+
+
+  describe '#full_name' do
+    it{expect(full_name).to eq 'Mr Bright Side'}
+  end
+
+  describe '#full_address' do
+    it{expect(full_address).to eq 'Little Street, Around the corner, Westin, Hotel, 1111, au'}
   end
 end
