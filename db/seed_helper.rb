007848fd81c209c599ac482768f6b71ebd81384d
iankhor <iankhorbc.thecoder@gmail.com>
2018-05-29 23:14:49 +1000

    add seed for patient provider assoc

--- db/seed_helper.rb
+++ db/seed_helper.rb
@@ -22,4 +22,37 @@ def seed_directory(total_records)
       )
     end
   end
+  puts "\n"
+
+end
+
+def seed_patient(total_records)
+  puts "Seeding patients with #{total_records} records ..."
+
+  Patient.transaction do
+    total_records.times do
+      print '.'
+      Patient.create!(
+        date_of_birth: Faker::Date.birthday(18, 90),
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+      )
+    end
+  end
+  puts "\n"
+end
+
+def associate_patient_providers(total_directory_records, total_patient_records, total_associations)
+  puts "Associating Patient and Providers with  #{total_associations} records ..."
+
+  DirectoriesPatients.transaction do
+    total_associations.times do
+      print '.'
+      DirectoriesPatients.create!(
+        patient_id: rand(1..(total_patient_records - 1)),
+        directory_id: rand(1..(total_directory_records - 1))
+      )
+    end
+  end
+  puts "\n"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
 require_relative './seed_helper'
 Faker::Config.locale = 'en-AU'
 
+PROVIDER_RECORDS = 30000
+PATIENT_RECORDS = 2000
+PROVIDER_PATIENT_ASSOC_RECORDS = rand(PATIENT_RECORDS..PROVIDER_RECORDS)
+
 puts "*" * 10 + 'SEED START' + "*" * 10
 
-seed_directory(30000)
+seed_directory(PROVIDER_RECORDS)
+seed_patient(PATIENT_RECORDS)
+associate_patient_providers(PROVIDER_RECORDS, PATIENT_RECORDS, PROVIDER_PATIENT_ASSOC_RECORDS)
 
 puts "*" * 10 + 'SEED END' + "*" * 10
